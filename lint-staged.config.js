28d405e9975a47f3be82363c9135acaeab070d77
Sudhanshu <perfectsudh@gmail.com>
2020-06-19 06:30:46 +0530

    Init: initialize a create-react-app with prettier/husky/lint-staged setup

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "*.+(js|jsx|ts|tsx)": ["prettier --write"],
+  "*.+(json|yml|yaml|md)": ["prettier --write"],
+  "*.+(s(a|c)ss|css)": ["prettier --write"],
+}
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react"
+function App() {
+  return <div>Hello World!!</div>
+}
+
+export default App
