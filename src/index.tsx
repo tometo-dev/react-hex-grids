363871cfe22c2650a15f5f19749ec955f5f5027e
Sudhanshu <perfectsudh@gmail.com>
2020-06-19 10:40:17 +0530

    Add rollup and package the app for publish

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
+import "./index.scss"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-)
+import HexGrids, { HexGridList, HexGridItem } from "./hex-grids"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+export default HexGrids
+export { HexGridList, HexGridItem }
